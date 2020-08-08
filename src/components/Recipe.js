026987d3ad10bf7b7a9d7c892ac0d8879a4d0adc
Milagros Alfonzo <milagrosalfonzo@outlook.com.ar>
2020-08-08 19:57:02 -0300

    listed recipes in the iu

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -6,6 +6,16 @@ const Recipe = ({ recipes }) => {
       <div className="col-md-4 mb-3">
         <div className="card">
           <h2 className="card-header">{recipes.strDrink}</h2>
+          <img
+            className="card-img-top"
+            src={recipes.strDrinkThumb}
+            alt={`Imagen de ${recipes.strDrink}`}
+          />
+          <div className="card-body">
+            <button type="button" className="btn btn-block btn-primary">
+              Ver receta
+            </button>
+          </div>
         </div>
       </div>
     </div>
